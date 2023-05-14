bfac0bc609f03e464975bba3142f4907536af8d5
Nihal00 <83398133+Nihal00@users.noreply.github.com>
2023-05-14 15:05:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,33 @@
 function letterCombinations(input_digit) {
-  //Complete the function
+ let map = {
+	 0: "0",
+	 1: "1",
+	 2: "abc",
+	 3: "def",
+	 4: "ghi",
+	 5: "jkl",
+	 6: "mno",
+	 7: "pqrs",
+	 8: "tuv",
+	 9: "wxyz"
+ }
+
+	let results = [];
+
+	function solve(input_digit, index, calculatedString) {
+
+		if(index >= seachString.length){
+			results.push(calculatedString);
+			return;
+		}
+		
+		let currString = map[seachString[index]]
+		for(let i = 0; i < currString.length; i++){
+			solve(seachString, index+1, calculatedString + currString);
+		}
+	}
+
+	solve(0, "");
 }
 
 module.exports = letterCombinations;
