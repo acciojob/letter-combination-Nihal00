7f84c40bfad82fe13ff42febca9cb97828e7fd0e
Nihal00 <83398133+Nihal00@users.noreply.github.com>
2023-05-14 17:33:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,69 +1,33 @@
-function letterCombinations(input_digit) {
 
-	if(input_digit.length === 0){
-		return [];
+function letterCombinationsRecur(digits, index, results) {
+	if(index === digits.length){
+		return results;
 	}
 
-	const mapping {
-		 2: "abc",
-		 3: "def",
-		 4: "ghi",
-		 5: "jkl",
-		 6: "mno",
-		 7: "pqrs",
-		 8: "tuv",
-		 9: "wxyz"
-	}
+	const mapping = {
+		2: "abc",
+	    3: "def",
+	    4: "ghi",
+	    5: "jkl",
+	    6: "mno",
+	    7: "pqrs",
+	    8: "tuv",
+	    9: "wxyz",
+	};
 
-	const results = [];
+	const letters = mapping[digits[index]];
 
-	for(const letter of mapping[input_digit[0]]) {
+	for(const letter of letters) {
 		results.push(letter);
+		letterCombinationsRecur(digits, index+1, results);
+		results.pop();
 	}
 
-	for(let i = 1; i < input_digit.length; i++){
-		const newResults = [];
-		for(const letter of results) {
-			for(const letter2 of mapping[input_digit[i]]) {
-				newResults.push(letter + letter2);
-			}
-		}
-		results = newResults;
-	}
-
-	results.sort();
 	return results;
+}
 
-	
- // let map = {
-	//  0: "0",
-	//  1: "1",
-	//  2: "abc",
-	//  3: "def",
-	//  4: "ghi",
-	//  5: "jkl",
-	//  6: "mno",
-	//  7: "pqrs",
-	//  8: "tuv",
-	//  9: "wxyz"
- // }
-
-	// let results = [];
-
-	// function solve(input_digit, index, calculatedString) {
-
-	// 	if(index >= seachString.length){
-	// 		results.push(calculatedString);
-	// 		return;
-	// 	}
-		
-	// 	let currString = map[seachString[index]]
-	// 	for(let i = 0; i < currString.length; i++){
-	// 		solve(seachString, index+1, calculatedString + currString);
-	// 	}
-	// }
-
-	// solve(0, "");
+function letterCombinations(input_digit) {
+	return letterCombinationsRecur(input_digit, 0, []);
 }
 
 module.exports = letterCombinations;
